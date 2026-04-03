7daa9aff266389eba31a6ddb48f0820ef15cd66c
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2026-04-02 22:05:54 -0300

    Implement send methods

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -5,7 +5,8 @@ import logging
 from .middleware import (
     MessageMiddlewareDisconnectedError,
     MessageMiddlewareQueue,
-    MessageMiddlewareExchange
+    MessageMiddlewareExchange,
+    MessageMiddlewareMessageError
 )
 
 logger = logging.getLogger(__name__)
@@ -30,6 +31,20 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
             logger.error(f"Error connecting to RabbitMQ: {e}")
             raise MessageMiddlewareDisconnectedError("Could not connect to RabbitMQ")
 
+    def send(self, message: str):
+        try:
+            if not self.channel:
+                raise MessageMiddlewareDisconnectedError("No active connection to RabbitMQ")
+            self.channel.basic_publish(
+                exchange='',
+                routing_key=self.queue_name,
+                body=message,
+                properties=pika.BasicProperties(delivery_mode=1),
+            )
+        except Exception as e:
+            logger.error(f"Error sending message: {e}")
+            raise MessageMiddlewareMessageError(f"Error sending message: {e}")
+
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host: str, exchange_name: str, routing_keys: list):
@@ -51,3 +66,21 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
         except pika.exceptions.AMQPConnectionError as e:
             logger.error(f"Error connecting to RabbitMQ: {e}")
             raise MessageMiddlewareDisconnectedError("Could not connect tos RabbitMQ")
+
+    def send(self, message: str, routing_key: str = None):
+        try:
+            if not self.channel:
+                raise MessageMiddlewareDisconnectedError("No active connection to RabbitMQ")
+
+            key = routing_key if routing_key else self.routing_keys[0]
+
+            self.channel.basic_publish(
+                exchange=self.exchange_name,
+                routing_key=key,
+                body=message,
+                properties=pika.BasicProperties(delivery_mode=2),
+            )
+            logger.info(f"Message published to exchange: {self.exchange_name} with routing key {key}: {message}")
+        except Exception as e:
+            logger.error(f"Error sending message: {e}")
+            raise MessageMiddlewareMessageError(f"Error sending message: {e}")
